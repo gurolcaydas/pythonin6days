dbfb9723e78154fde304bc8302f088b2a1bed0b1
Gurol <gurol@caydas.com>
2024-02-02 13:49:15 +0300

    MasterMind

--- first.py
+++ first.py
@@ -28,7 +28,7 @@ j="Falan {1} \nfilan {0}"
 # print(j.format(b,a).upper())
 ## string methods çalışılacak.
 ## https://www.w3schools.com/python/python_strings_methods.asp
-ilkListe = ["1","2","3","2"]
+ilkListe = ["1",2,3,"2"]
 # print (ilkListe[0:2])
 # print (ilkListe.count("2"))
 # print(type(ilkListe))
@@ -41,11 +41,46 @@ ilkListe[2]="4"
 ilkListe.insert(1,"0")
 ilkListe.append("5")
 # print(ilkListe)
-ikinciListe=["A","B","C"]
+ikinciListe=["Adana","Istanbul","Ankara","B","Rize"]
 ilkListe.extend(ikinciListe)
 ilkListe.pop(1)
 print(ilkListe)
-for x in ilkListe:
-    print(x)
+# for x in ilkListe:
+#     print(x)
+# [print(x) for x in ilkListe]
+yeniListe = [x for x in ilkListe if isinstance(x, str) and "n" in x]
+print(yeniListe)
+# yeniListe = [x for x in ilkListe if type(x) is not int]
+# print (len(yeniListe))
+yeniListe.sort()
+print(yeniListe)
+thistuple = tuple(("apple", "banana", "cherry", "apple", "cherry"))
+print(thistuple)
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+# for x in thisdict:
+#   print(thisdict[x])
+# for x, y in thisdict.items():
+#   print("key: ",x," value: ", y)
+# for x in range(2, 6):
+#   print(x)
+kup = lambda a,b : a**b
+print (kup(2,3))
+
+class teneke:
+    def __init__(self,ad,soyad):
+        self.ad = ad
+        self.soyad = soyad
+    def bilgileri(self):
+        print(self.ad,self.soyad)
+t1 = teneke("adı","soyadı")
+t1.bilgileri()
+print(t1.ad)
 
 
---
+++ mastermind.py
@@ -0,0 +1,129 @@
+import os
+import datetime
+import random
+
+def temizle():
+    # For Windows
+    if (os.name == 'nt'):
+        os.system('cls')
+    # For macOS and Linux
+    else:
+        os.system('clear')   
+def dorkBasamakli(numara):
+    if len(numara) != 4:
+        return False
+    return True
+def farkliHarfler(numara):
+    for x in numara:
+        if (numara.count(x)>1):
+            return False
+    return True
+def sifirBesArasi(numara):
+    for x in numara:
+        if (x not in "123450"):
+            return False
+    return True
+def bulmaca(): 
+    sayiKumesi="012345"
+    gizli = ""
+    while (len(gizli) < 4):
+        siradaki=random.randrange(6)
+        if (sayiKumesi[siradaki] not in gizli):
+            gizli = sayiKumesi[siradaki] + gizli 
+    return gizli
+def kaciVarYeriYanlis(numara):
+    buVar = 0
+    for x in numara:
+        if (x in bulBeni):
+            buVar += 1
+    return buVar
+
+def kaciVarYeriDogru(numara):
+    buVar = 0
+    y=0
+    for x in numara:
+        if (x == bulBeni[y]):
+            buVar += 1
+        y += 1
+    return buVar
+
+def ekraniYazdir():
+    for (x,y) in zip(tahminler, sonuclar):
+        print(x,y)
+print("--------------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------")
+print (datetime.datetime.now().strftime("%d of %B"))
+username = input("\nİsminiz: ")
+mesaj= " Oyunumuza hoşgeldiniz, "+username+" "
+uzunluk=int((80-len(mesaj))/2)
+ondeki="-"*uzunluk 
+arkadaki="-"*(uzunluk + len(mesaj) % 2)
+tahminler = list(())
+sonuclar = list(())
+degil4Basamak = 0
+degilRakamlarFarkli = 0
+degilSifirBes = 0
+bulBeni=bulmaca()
+tahminSayisi = 0
+temizle()
+print("--------------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------")
+print("------------------------------- MasterMind Game --------------------------------")
+print("--------------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------")
+print(ondeki+ mesaj +arkadaki)
+print("--------------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------")
+print("Senin için 4 basamaklı bir şifre tuttuk, tahmin edebilir misin?")
+print("Acemi Python programcısının eksikleri yüzünden bu 4 basamak renklerden değil rakamlardan oluşuyor.")
+print("Master Mind oyunun gereği olarak sadece 6 çeşit renk yani bu örnekte rakam kullandık")
+print("Yani 0-5 rakamlarıdan oluşan, birbirinin aynı rakamları içermeyen, 4 basamaklı bir sifre olacak")
+while(True):
+    tahmin = input("\nTahmininizi giriniz:")
+    if dorkBasamakli(tahmin):
+        if farkliHarfler(tahmin):
+            if sifirBesArasi(tahmin):
+                tahminSayisi += 1
+                yeriDogrular=kaciVarYeriDogru(tahmin)
+                olanlar=kaciVarYeriYanlis(tahmin) - yeriDogrular
+                sonuclar.append("-"+format(olanlar)+" +"+format(yeriDogrular))
+                tahminler.append(tahmin)
+                ekraniYazdir()
+                if yeriDogrular == 4:
+                    print("--------------------------------------------------------------------------------")
+                    print("--------------------------------------------------------------------------------")
+                    print("Tebrikler, bulmacayı {}. tahminde buldunuz".format(tahminSayisi))
+                    print("--------------------------------------------------------------------------------")
+                    print("--------------------------------------------------------------------------------")
+                    print("Ama bir dakika?!")
+                    toplamHataliGiris = degil4Basamak+degilRakamlarFarkli+degilSifirBes
+                    if (toplamHataliGiris == 0):
+                        print("Sen hiç hatalı giriş yapmamışsın? Bravo kuralcı kişi.")
+                    else:
+                        if(toplamHataliGiris < 5):
+                            print("Az miktarda hatalı girişi her hoca olumlulukla kabul eder.")
+                        else:
+                            print("Biraz abartılı hatalı giriş olmuş.")
+                            if (toplamHataliGiris > 10):
+                                print("Bacım sen rastgele basmışsın tuşlara, oynamayacaksan oynamıyorum de, ktrl-C basıp kaçsaydın. Hadi kardeşim CPU'yu oyalama!")
+                            else:
+                                print("Gereksiz taramalardan kaçınalım!")
+                    print("--------------------------------------------------------------------------------")
+                    print("----------------------------------------------------------------------------end-")
+                    break
+
+            else:
+                degilSifirBes += 1
+                print("Lütfen 0-5 rakamları den olusan 4 basamakli bir sayi giriniz.")
+        else:
+            degilRakamlarFarkli += 1
+            print("Amaaaa lütfen rakamlar birbirinden farklı olsun.")
+    else:
+        degil4Basamak += 1
+        print("Lütfen 4 basamakli bir sayi giriniz")
+        if degil4Basamak>5:
+            print("Bu iş can sıkmaya başladı, 4 basamak nedir bilir misin?")
+
+
+
